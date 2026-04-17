c00eede3f0690edf0699b1b237f749ff56ebafaf
asid_dev01 <getazzam@gmail.com>
2026-04-17 15:41:38 +0700

    update init

--- src/init.ts
+++ src/init.ts
@@ -5,30 +5,26 @@ import {
   viewport,
   init as initSDK,
   mockTelegramEnv,
-  type ThemeParams,
   retrieveLaunchParams,
   emitEvent,
   miniApp,
   backButton,
-} from '@tma.js/sdk-react';
+} from "@tma.js/sdk-react";
 
 /**
  * Initializes the application and configures its dependencies.
  */
-export async function init(options: {
-  debug: boolean;
-  eruda: boolean;
-  mockForMacOS: boolean;
-}): Promise<void> {
+export async function init(options: { debug: boolean; eruda: boolean; mockForMacOS: boolean }): Promise<void> {
   // Set @telegram-apps/sdk-react debug mode and initialize it.
   setDebug(options.debug);
   initSDK();
 
   // Add Eruda if needed.
-  options.eruda && void import('eruda').then(({ default: eruda }) => {
-    eruda.init();
-    eruda.position({ x: window.innerWidth - 50, y: 0 });
-  });
+  options.eruda &&
+    void import("eruda").then(({ default: eruda }) => {
+      eruda.init();
+      eruda.position({ x: window.innerWidth - 50, y: 0 });
+    });
 
   // Telegram for macOS has a ton of bugs, including cases, when the client doesn't
   // even response to the "web_app_request_theme" method. It also generates an incorrect
@@ -37,19 +33,20 @@ export async function init(options: {
     let firstThemeSent = false;
     mockTelegramEnv({
       onEvent(event, next) {
-        if (event.name === 'web_app_request_theme') {
-          let tp: ThemeParams = {};
+        if (event.name === "web_app_request_theme") {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          let tp: any = {};
           if (firstThemeSent) {
             tp = themeParams.state();
           } else {
             firstThemeSent = true;
-            tp ||= retrieveLaunchParams().tgWebAppThemeParams;
+            tp = retrieveLaunchParams().tgWebAppThemeParams || tp;
           }
-          return emitEvent('theme_changed', { theme_params: tp });
+          return emitEvent("theme_changed", { theme_params: tp });
         }
 
-        if (event.name === 'web_app_request_safe_area') {
-          return emitEvent('safe_area_changed', { left: 0, top: 0, right: 0, bottom: 0 });
+        if (event.name === "web_app_request_safe_area") {
+          return emitEvent("safe_area_changed", { left: 0, top: 0, right: 0, bottom: 0 });
         }
 
         next();
